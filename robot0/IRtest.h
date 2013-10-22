bd90067daac54e95085c39a2d141077e23816fc9
RoboNuc <alecj@iastate.edu>
2013-10-22 16:32:06 +0200

    Added some other control code/ alfred

--- robot0/IRtest.h
+++ robot0/IRtest.h
@@ -8,9 +8,10 @@ const double beta_2 = -14; 			// constant for calibration
 const double Sharp2Wheel_FrontRightD = 6.0;	// distance from sharp ch8 to wheel
 const double Sharp2Wheel_RearRightD = 6.5;	// distance from sharp ch4 to wheel
 const double IR_rightD = 16.2;		// distance between ch4 and ch8.
-const double K_x = 0.5; 			// Control coefficient --> ask GB.
+const double K_forward = 0.5; 			// Control coefficient --> ask GB.
 const double Wheel2Wall_D = 7; 		// distance from the wheel to a wall.
-const double k_theta = 4;			// Control coefficient --> Ask GB.
+const double k_theta = 7;			// Control coefficient --> Ask GB.
 const double RobotSpeed = 6;		// Constant speed of the robot.
+const double MaxFowardAngle = 0.2;	// Max angle where the robot is allowed to move forward in radians.
 
 #endif /* IRTEST_H_ */
