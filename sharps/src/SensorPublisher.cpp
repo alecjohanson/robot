425b43ccc5c123956e915d2ea5ab2a5ab65d489d
RoboNuc <alecj@iastate.edu>
2013-11-04 11:50:56 +0100

    All sensors working correctly.

--- sharps/src/SensorPublisher.cpp
+++ sharps/src/SensorPublisher.cpp
@@ -9,6 +9,25 @@ using namespace differential_drive;
 static ros::Publisher sharps_pub;
 static ros::Subscriber arduino_sub;
 
+
+
+double ADCtoCM(double params[4], double SensorData){
+	// Receives 4 parameters
+	// param[0] = alpha constant
+	// param[1] = beta constant
+	// param[2] = Distance from sharp to the wheel.
+	// param[3] = max distance showed by the sharp.
+	// Example of an equation would be: 
+
+	//FrontRightSensor = alpha_1/(FrontRightSensor+beta_1)-Sharp2Wheel_FrontRightD;	
+	double SensorCM = params[0]/ ( SensorData - params[1] ) - params[2];
+	if (SensorCM < 0)
+		SensorCM = 0;
+	else if (SensorCM > params[3])
+		SensorCM = params[3];
+	return SensorCM;
+}
+
 void getDistance(const differential_drive::AnalogC &msg){
    
   sharps::Distance dst;
@@ -26,27 +45,35 @@ void getDistance(const differential_drive::AnalogC &msg){
   FrontLeftSensor = msg.ch7;
 
   // Getting the info from the Front and Rear.
-  RearSensor = msg.ch2;
-  FrontSensor = msg.ch3;
+  RearSensor = msg.ch3;
+  FrontSensor = msg.ch2;
 
   // Conversion from the ADC to cm.
-  FrontRightSensor = alpha_1/(FrontRightSensor+beta_1)-Sharp2Wheel_FrontRightD;
-  RearRightSensor = alpha_2/(RearRightSensor+beta_2)-Sharp2Wheel_RearRightD;
+  double parameters[] = {alpha_1, beta_1, Sharp2Wheel_FrontRightD, MaxDist};
 
-  FrontLeftSensor = alpha_3/(FrontLeftSensor+beta_3)-Sharp2Wheel_FrontLeftD;
-  RearLeftSensor = alpha_4/(RearLeftSensor+beta_4)-Sharp2Wheel_RearLeftD;
-  
-  RearSensor = alpha_5 / (RearSensor+beta_5) - Sharp2Edge_RearD;
-  FrontSensor = alpha_6 / (FrontSensor+beta_6) - Sharp2Edge_FrontD;
+  //FrontRightSensor = alpha_1/(FrontRightSensor+beta_1)-Sharp2Wheel_FrontRightD;
+  FrontRightSensor = ADCtoCM(parameters, FrontRightSensor);
 
-  // Debugg output
-  // std::cerr<<'Front Right '<<FrontRightSensor<<std::endl;
-  // std::cerr<<'Rear Right  '<<RearRightSensor<<std::endl;
-  // std::cerr<<'Front Left  '<<FrontLeftSensor<<std::endl;
-  // std::cerr<<'Rear Left   '<<RearLeftSensor<<std::endl;
-  // std::cerr<<'Front       '<<FrontSensor<<std::endl;
-  // std::cerr<<'Rear        '<<RearSensor<<std::endl;
-  
+  //RearRightSensor = alpha_2/(RearRightSensor+beta_2)-Sharp2Wheel_RearRightD;
+  parameters[2] = Sharp2Wheel_RearRightD;
+  RearRightSensor = ADCtoCM(parameters, RearRightSensor);
+
+  //FrontLeftSensor = alpha_3/(FrontLeftSensor+beta_3)-Sharp2Wheel_FrontLeftD;
+  parameters[2] = Sharp2Wheel_FrontLeftD;
+  FrontLeftSensor = ADCtoCM(parameters, FrontLeftSensor);
+
+  //RearLeftSensor = alpha_4/(RearLeftSensor+beta_4)-Sharp2Wheel_RearLeftD;
+  parameters[2] = Sharp2Wheel_RearLeftD;
+  RearLeftSensor = ADCtoCM(parameters, RearLeftSensor);
+
+  //RearSensor = alpha_5 / (RearSensor+beta_5) - Sharp2Edge_RearD;
+  parameters[2] = Sharp2Edge_RearD;
+  RearSensor = ADCtoCM(parameters, RearSensor);
+
+  //FrontSensor = alpha_6 / (FrontSensor+beta_6) - Sharp2Edge_FrontD;
+  parameters[2] = Sharp2Edge_FrontD;
+  FrontSensor = ADCtoCM(parameters, FrontSensor);
+
   // Message http://wiki.ros.org/rosbuild/msg
   
   // Publish into the Sharp Distance topic.
@@ -66,7 +93,7 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "SensorPublisher");
   ros::NodeHandle nh;
-// ***********
+
   sharps_pub = nh.advertise<sharps::Distance>("/sharps/Distance", 1);
   arduino_sub = nh.subscribe("/sensors/ADC/",1,getDistance);
   ros::Rate loop_rate(100);
--- sharps/src/SensorPublisher.h
+++ sharps/src/SensorPublisher.h
@@ -35,12 +35,18 @@ const double beta_6 = -14;                   // constant for calibration
 
 // Distance to remove from measurement
 const double Sharp2Wheel_FrontRightD = 5.82;  // distance from sharp ch8 to wheel
-const double Sharp2Wheel_RearRightD = 6.14; //6.35;   // distance from sharp ch4 to wheel
+const double Sharp2Wheel_RearRightD = 6.14;   // distance from sharp ch4 to wheel
 
 const double Sharp2Wheel_FrontLeftD = 5.04;     // distance from sharp ch7 to wheel
 const double Sharp2Wheel_RearLeftD = 6.33;      // distance from sharp ch5 to wheel
 
-const double Sharp2Edge_RearD = 3;           // distance from sharp ch2 to edge
-const double Sharp2Edge_FrontD = 0;          // sharp ch3 to edge
+const double Sharp2Edge_RearD = 4.62;           // distance from sharp ch3 to edge
+const double Sharp2Edge_FrontD = 0;          // sharp ch2 to edge
+
+// Max distance to show
+const double MaxDist = 20;
+//double SensorData;
+//double params[4];
+//double [] parameters;
 
 #endif /* SENSORPUBLISHER_H_ */
